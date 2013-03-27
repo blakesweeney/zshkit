216608edd684308552314290daa44ed2f1b7b217
Blake Sweeney <blakes.85@gmail.com>
2013-03-27 09:48:25 -0400

    Attempt to prepend rbenv.

--- 07_rbenv.zsh
+++ 07_rbenv.zsh
@@ -1,10 +1,13 @@
-if [[ -d "$HOME/.rbenv" ]]; then
-  rb_complete="$HOME/.rbenv/completions/rbenv.zsh"
+if (( $+commands[rbenv] )); then
+  rb_root=`rbenv root`
+
+  rb_complete="$rb_root/completions/rbenv.zsh"
   if [[ -e "$rb_complete" ]]; then
     source "$rb_complete"
   fi
 
-  PATH="$HOME/.rbenv/bin":$PATH
+  path[1,0]="$rb_root/bin"
+
   eval "$(rbenv init -)"
   function ruby-version-prompt () {
     echo "$(rbenv version-name) "
