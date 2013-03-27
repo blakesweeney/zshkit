fdcfe55e74080ba99194e30e464b8eae29292e01
Blake Sweeney <blakes.85@gmail.com>
2013-03-27 09:16:48 -0400

    Make path contain unique and existing dirs.

--- 01_path.zsh
+++ 01_path.zsh
@@ -1,6 +1,10 @@
+typeset -U path
+
 path=($path /usr/local/bin $HOME/.local/bin)
 BREW_HOME=`brew --prefix`
 
 path=($HOME/bin $HOME/Applications/scripts $path)
 path=($path /usr/texbin /usr/sbin)
 path=($path $BREW_HOME/share/python $BREW_HOME/share/python3 $BREW_HOME/share/npm/bin)
+
+path=($^path(N))
