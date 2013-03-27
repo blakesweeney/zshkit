a1bb2f7c1a881cf57c3d991ce9a224c11966a024
Blake Sweeney <blakes.85@gmail.com>
2013-03-27 09:17:16 -0400

    Notes on where some changes are from.

--- 01_path.zsh
+++ 01_path.zsh
@@ -1,3 +1,5 @@
+# From:
+# http://unix.stackexchange.com/questions/62579/is-there-a-way-to-add-a-directory-to-my-path-in-zsh-only-if-its-not-already-pre
 typeset -U path
 
 path=($path /usr/local/bin $HOME/.local/bin)
@@ -7,4 +9,6 @@ path=($HOME/bin $HOME/Applications/scripts $path)
 path=($path /usr/texbin /usr/sbin)
 path=($path $BREW_HOME/share/python $BREW_HOME/share/python3 $BREW_HOME/share/npm/bin)
 
+# From:
+# http://stackoverflow.com/questions/9347478/how-to-edit-path-variable-in-zsh
 path=($^path(N))
