ff81fd857188193a6a9b7a8502977ffc2541fe9c
Blake Sweeney <blakes.85@gmail.com>
2013-03-27 09:41:25 -0400

    Use commands to find virtualenvwrapper.sh

--- 07_virtualenv.zsh
+++ 07_virtualenv.zsh
@@ -1,4 +1,4 @@
 export WORKON_HOME="$HOME/.pyenv/"
 BREW_HOME=`brew --prefix`
 export VIRTUALENVWRAPPER_PYTHON="$BREW_HOME/bin/python"
-source /usr/local/bin/virtualenvwrapper.sh
+source "$commands[virtualenvwrapper.sh]"
