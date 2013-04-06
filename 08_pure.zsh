45e73ca3b7d045f4ca7262d553339911b0242ce8
Blake Sweeney <blakes.85@gmail.com>
2013-04-05 22:15:18 -0400

    Check if pure exists.

--- 08_pure.zsh
+++ 08_pure.zsh
@@ -1,3 +1,5 @@
-export PURE_INCLUDE="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
-export PURE_LIBRARY="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
-export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/lib/pure"
+if (( $+commands[pure] )); then
+  export PURE_INCLUDE="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
+  export PURE_LIBRARY="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
+  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/lib/pure"
+fi
