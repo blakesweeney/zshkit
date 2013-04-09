12df0867a34418c14a21f15004cb4faa2d82a64f
Blake Sweeney <blakes.85@gmail.com>
2013-04-08 21:50:07 -0400

    Use old rbenv versions.

--- 07_chruby.zsh
+++ 07_chruby.zsh
@@ -1,14 +1,15 @@
 if (( $+commands[brew] )); then
   base=`brew --prefix`
-  chruby=$base/opt/chruby/share/chruby/chruby.sh
-
-  rbs=$HOME/.rbenv/versions/*
-  if [[ -d $rbs ]]; then
-    RUBIES=($rbs)
-  fi
+  chruby="$base/opt/chruby/share/chruby/chruby.sh"
 
   if [[ -e $chruby ]]; then
     source $chruby
     source $base/opt/chruby/share/chruby/auto.sh
   fi
+
+  rbs="$HOME/.rbenv/versions"
+  if [[ -d "$rbs" ]]; then
+    RUBIES=($rbs/*)
+  fi
+
 fi
