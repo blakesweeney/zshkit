1d0864d9ef8d93a4c3a3f4b781e2095ae73d8750
Blake Sweeney <blakes.85@gmail.com>
2013-04-11 13:01:10 -0400

    Don't mess with DYLIB

--- 08_pure.zsh
+++ 08_pure.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[pure] )); then
   export PURE_INCLUDE="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
   export PURE_LIBRARY="./:./src/:/usr/local/Cellar/pure/0.53/lib/pure"
-  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/lib/pure"
+  #export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/lib/pure"
 fi
