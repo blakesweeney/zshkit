8e230de3b35c85b7b6e2d6365c21dfd147a2adf3
Blake Sweeney <blakes.85@gmail.com>
2013-04-12 09:19:17 -0400

    Add subupdate function.

--- 12_subs.zsh
+++ 12_subs.zsh
@@ -1,13 +1,15 @@
 export SUB_HOME="$HOME/.subs"
 
+function sub-update {
+  . $SUB_HOME/init
+}
+
 if [[ ! -d "$SUB_HOME/bin" ]]; then
   mkdir -p "$SUB_HOME/bin"
 fi
 
-path+="$SUB_HOME/bin"
-
 if [[ -e "$SUB_HOME/init" ]]; then
-  . $SUB_HOME/init
+  sub-update
 fi
 
 alias n='_nowhere_wrapper cd'
