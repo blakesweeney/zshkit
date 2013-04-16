14ee731a47e2cb046803588803333e8bd8164ef9
Blake Sweeney <blakes.85@gmail.com>
2013-04-16 11:37:38 -0400

    Check that brew exists before using it to setup virtualenv

--- 07_virtualenv.zsh
+++ 07_virtualenv.zsh
@@ -1,4 +1,6 @@
-export WORKON_HOME="$HOME/.pyenv/"
-BREW_HOME=`brew --prefix`
-export VIRTUALENVWRAPPER_PYTHON="$BREW_HOME/bin/python"
-source "$commands[virtualenvwrapper.sh]"
+if (( $+commands[brew ] )); then
+  export WORKON_HOME="$HOME/.pyenv/"
+  BREW_HOME=`brew --prefix`
+  export VIRTUALENVWRAPPER_PYTHON="$BREW_HOME/bin/python"
+  source "$commands[virtualenvwrapper.sh]"
+fi
